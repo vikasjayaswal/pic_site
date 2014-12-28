1e2a9bb0c60c32a955d1605ecf39d39166e5b81c
Vic Jay <vikas.jayaswal@gmail.com>
2014-12-28 17:34:48 -0500

    Download Jquery
    Donwload JQuery, Modified Javascript so Toggles Work, Modified view so
    CSS works

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-    $(".icon-file-picture").click(function() {
+    $(".icon-file-pdf").click(function() {
         $(this).toggleClass("active");
+        $(".caption-file").toggleClass("active");
         $(".icon-earth").toggleClass("active");
-        $(".file_paragraph").toggleClass("active");
+        $(".caption-url").toggleClass("active");
         $(".address").toggleClass("hidden");
         $(".file_dialog").toggleClass("hidden");
     });
     $(".icon-earth").click(function() {
         $(this).toggleClass("active");
-        $(".icon-file-picture").toggleClass("active");
+        $(".caption-url").toggleClass("active");
+        $(".icon-file-pdf").toggleClass("active");
+        $(".caption-file").toggleClass("active");
         $(".address").toggleClass("hidden");
         $(".file_dialog").toggleClass("hidden");
     });
